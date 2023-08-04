61532032c94394a464e3d7dbfb42af5c1b936934
lexey <tblz@palavara.com>
2023-08-04 23:58:44 +0200

    remove lang reset in the langWatcherSaga

--- src/sagas/langWatcherSaga.ts
+++ src/sagas/langWatcherSaga.ts
@@ -1,11 +1,8 @@
 import { PayloadAction } from "@reduxjs/toolkit";
 import {call, take} from "redux-saga/effects";
 import {actions} from "../actions";
-import {ELang} from "../services/i18n";
 
 export function* langWatcherSaga(i18n:any){
-  yield call(i18n.changeLanguage, ELang.RU )
-
   while(true){
     const {payload}:PayloadAction<string> = yield take(actions.changeLanguage.type)
     yield call(i18n.changeLanguage, payload)
