b740f16ec914713cf6b33ba71cad3b553609fd25
lexey <tblz@palavara.com>
2023-10-29 12:36:06 +0100

    css tweaks

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ export const config = {
   sentry: process.env.REACT_APP_SENTRY,
   imgPrefix: process.env.REACT_APP_IMG_PREFIX,
   lqipPrefix: 'lqip',
-  mobileBreakpoint: 700
+  mobileBreakpoint: 600
 }
